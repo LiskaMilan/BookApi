73c5f1b531a1a4a98aa0c2ca79abd2f373d71c5c
Monty <milan.liska1994@gmail.com>
2024-05-23 16:15:31 +0200

    Add validation on year
    year

--- Books/Models/Book.cs
+++ Books/Models/Book.cs
@@ -9,6 +9,7 @@ namespace Books.Models
         public string Title { get; set; }
         public string Author { get; set; }
         [Required(ErrorMessage = "Year is required")]
+        [Range(1000, 9999, ErrorMessage = "Year must be between 1000 and 9999")]
         public int Year { get; set; }
     }
 }
